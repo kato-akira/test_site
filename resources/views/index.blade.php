312c968e23445d99c320f25e42f6e3024ce706f0
kato <kato.akira.work@gmail.com>
2026-09-24 01:14:04 +0900

    aaa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
         <p>トップページ</p>
         
         <!-- route('register') を使って /register へ遷移するボタン -->
-        <a href="{{ route('UserList') }}">アカウント管理</a>
+        <a href="{{ route('UserList') }}">アカウント　管理</a>
     </div>
 </body>
 </html>
